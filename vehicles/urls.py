51078cfa58763042b211fb342bfaa1af0dbda88b
ian-IBCIRL <ian@ibcirl.com>
2023-02-02 23:10:37 +0000

    renamed project to match home page

--- vehicles/urls.py
+++ vehicles/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path("", views.PostList.as_view(), name="home"),
     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
     path('like/<slug:slug>', views.PostLike.as_view(), name='post_like'),
-    path('post_vehicle/<slug:slug>', views.PostDetail.as_view(), name='post_vehicle'),
+#    path('post_vehicle/<slug:slug>', views.PostDetail.as_view(), name='post_vehicle'),
 #   path('edit_post/<post_id>',
 #        views.add_edit_post, name='add_edit_post'),
 #   path('delete_post/<post_id>',
