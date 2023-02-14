d0d8fe3c506f00b6a20169657f62a7463a2bc4ba
ian-IBCIRL <ian@ibcirl.com>
2023-02-14 19:11:42 +0000

    adjusted main menu dropdowns

--- vehicles/urls.py
+++ vehicles/urls.py
@@ -8,10 +8,10 @@ urlpatterns = [
 #    path("admin2", views.admin.as_view(), name="admin2"),
 #    path("admin/vehicles/post", admin.vehicles.post.as_view(), name="admin_post"),    # noqa
     path('delete/<post_id>', views.delete_post, name='delete'),
-    path('post_vehicle_view/', post_vehicle_view, name='post_vehicle_view'),
+    path('post_vehicle_view/', views.post_vehicle_view, name='post_vehicle_view'),
     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
     path('like/<slug:slug>', views.PostLike.as_view(), name='post_like'),
-    path('post_vehicle', views.PostVehicle.as_view(), name='post_vehicle'),
+    path('post_vehicle/', views.PostVehicle.as_view(), name='post_vehicle'),
 
     #   path('edit_post/<post_id>',
     #        views.add_edit_post, name='add_edit_post'),
