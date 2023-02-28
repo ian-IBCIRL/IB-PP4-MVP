9f59054081463182760468f513af3d4b1170d85b
ian-IBCIRL <ian@ibcirl.com>
2023-02-28 17:57:49 +0000

    updated create method to class based option

--- vehicles/views.py
+++ vehicles/views.py
@@ -8,11 +8,46 @@ from .forms import CommentForm, PostVehicleForm, VehicleForm
 from .forms import SparePartForm, PostForm
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
+from django.utils.text import slugify
 from django.contrib.messages.views import SuccessMessageMixin
 from django.contrib.auth.mixins import (
     LoginRequiredMixin,
     UserPassesTestMixin
     )
+
+
+class Text:
+    #  https://lookaway.info/zine/information/prepopulate-a-form-field-using-slugs-and-integers-django-20210410/
+
+    def slugify_unique(model, title):
+        '''
+        Given a DB model and a title, return a unique slug that is unique \
+        to all other slug fields of the given DB model.
+
+        Arguments
+        model - Must be a Django database model that has \
+                   a slug field called "slug".
+        title - The string used to create the slug.
+
+        Returns - A slug that is unique across all instances of the model.
+        '''
+        from django.utils.text import slugify
+        slug = slugify(title)
+        existing_slugs = []
+        try:
+            [existing_slugs.append(str(i.slug)) for i in model.objects.all()]
+        except:
+            print("There was no slug field found for {}".format(model))
+            return slug
+        if slug in existing_slugs:
+            date_slug = slug + "-" + timezone.now().strftime("%Y%m%d")
+            if date_slug in existing_slugs:
+                long_slug = date_slug + timezone.now().strftime("%m%s")
+                return long_slug
+            else:
+                return date_slug
+        else:
+            return slug
 
 
 class PostList(generic.ListView):
@@ -181,11 +216,12 @@ def addVehicle(request):
 class PostCreateView(LoginRequiredMixin, CreateView):
     model = Post
     fields = [
-        'title', 'slug', 'featured_image', 'excerpt', 'content',
+        'title', 'excerpt', 'featured_image', 'content',
         'status', 'youtube_link']
 
     def form_valid(self, form):
         form.instance.author = self.request.user
+        form.instance.slug = Text.slugify_unique(self.model, form.instance.title)  # noqa
         return super().form_valid(form)
 
 
@@ -203,7 +239,9 @@ class PostUpdateView(
     def form_valid(self, form):
         form.instance.author = self.request.user
         post = self.get_object()
-        form.instance.slug = post.slug
+        form.instance.slug = Text.slugify_unique(self.model, form.instance.title)  # noqa
+
+#        form.instance.slug = post.slug
         return super().form_valid(form)
 
     def test_func(self):
